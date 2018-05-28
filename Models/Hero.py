ab740b8a64a0ffdebed6e902fe9d6a30f34858a0
Tanguy Le Roux <leroux.tanguy.51@gmail.com>
2018-05-28 15:45:39 +0200

    Hero class modified

--- Models/Hero.py
+++ Models/Hero.py
@@ -1,3 +1,5 @@
+import json
+
 class Hero: # Définition de notre classe Hero 
     """Classe définissant un hero caractérisée par :
     - life
@@ -7,10 +9,26 @@ class Hero: # Définition de notre classe Hero
     - speed
     - position"""
 
-    def __init__(self, name = 'heroe', life = 50, strength = 5, speed = 1, canFly = false, position = (2, 5)): # Notre méthode constructeur
+    def __init__(self, name = 'heroe', life = 50, strength = 5, speed = 1, canFly = False, position = (2, 5)): # Notre méthode constructeur
         self.name     = name
         self.life     = life
         self.strength = strength
         self.speed    = speed
         self.canFly   = canFly
         self.position = position
+
+    def __str__(self):
+        return "Hero instance with name is%s"%self.name
+
+    @classmethod
+    def getByConf(cls, name = 'superman'):
+        life = null
+
+        with open('../conf/heroes.json') as confs_file:
+            confs = json.load(confs_file)
+
+            if name in confs:
+                conf = confs[name]
+
+                if 'life' in conf:
+                    print(conf['life'])
